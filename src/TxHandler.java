f32916a5cebb6d7dd26035d14f7dc68c16577ef3
Alexander Wilson <wilsonalexanderjohn@byu.net>
2019-03-15 20:12:03 -0600

    Finished assignment. Attempted extra credit but ran out of time.

--- src/TxHandler.java
+++ src/TxHandler.java
@@ -1,4 +1,8 @@
+import java.util.*;
+
 public class TxHandler {
+
+    private UTXOPool unspentTransactions;
 
     /**
      * Creates a public ledger whose current UTXOPool (collection of unspent transaction outputs) is
@@ -6,7 +10,7 @@ public class TxHandler {
      * constructor.
      */
     public TxHandler(UTXOPool utxoPool) {
-        // IMPLEMENT THIS
+        unspentTransactions = new UTXOPool(utxoPool);
     }
 
     /**
@@ -19,7 +23,97 @@ public class TxHandler {
      *     values; and false otherwise.
      */
     public boolean isValidTx(Transaction tx) {
-        // IMPLEMENT THIS
+
+        HashMap<UTXO, Transaction.Output> newlyCreatedTransactions = new HashMap<>();
+        HashMap<UTXO, Transaction.Output> newlySpentTransactions = new HashMap<>();
+        double totalOutputValue = 0;
+        double totalInputValue = 0;
+        boolean validTransaction = true;
+
+        if(tx.numInputs() == 0)
+        {
+            return false;
+        }
+
+        //save these unspent transactions until they are all confirmed, removing them from the unspent transaction pool as you go.
+        // If the transaction is invalid, add them back into the set of unspent transactions.
+        Transaction.Input currentInput;
+
+        for (int inputIndex = 0; inputIndex < tx.numInputs(); inputIndex++)
+        {
+            currentInput = tx.getInput(inputIndex);
+
+            UTXO unspentTransactionReference = new UTXO(currentInput.prevTxHash, currentInput.outputIndex);
+
+            Transaction.Output output = unspentTransactions.getTxOutput(unspentTransactionReference);
+
+            if(output != null)
+            {
+                if(Crypto.verifySignature(output.address, tx.getRawDataToSign(inputIndex), currentInput.signature))
+                {
+                    //aggregate input values and add to the set of newly spent transaction outputs after determining them to be valid
+                    totalInputValue += output.value;
+
+                    newlySpentTransactions.put(unspentTransactionReference, output);
+                    unspentTransactions.removeUTXO(unspentTransactionReference);
+                }
+                else
+                {
+                    validTransaction = false;
+                    break;
+                }
+            }
+            else
+            {
+                validTransaction = false;
+                break;
+            }
+        }
+
+        if(validTransaction)
+        {
+            Transaction.Output currentOutput;
+
+            //verify outputs are positive and aggregate value to verify later that in total they are <= sum of the inputs
+            for (int outputIndex = 0; outputIndex < tx.numOutputs(); outputIndex++) {
+                currentOutput = tx.getOutput(outputIndex);
+
+                if (currentOutput.value > 0) {
+                    totalOutputValue += currentOutput.value;
+                    //add to set of newly created transaction outputs
+                    newlyCreatedTransactions.put(new UTXO(tx.getHash(), outputIndex), currentOutput);
+                } else {
+                    validTransaction = false;
+                    break;
+                }
+
+            }
+        }
+
+        //make sure outputs don't exceed inputs
+        if(validTransaction && totalOutputValue > totalInputValue)
+        {
+            validTransaction = false;
+        }
+
+        if(validTransaction)
+        {
+            //transaction is confirmed valid, add newly spent transaction outputs and newly created transaction outputs to their respective pools
+            for(UTXO newlyCreatedUnspentTransaction : newlyCreatedTransactions.keySet())
+            {
+                unspentTransactions.addUTXO(newlyCreatedUnspentTransaction, newlyCreatedTransactions.get(newlyCreatedUnspentTransaction));
+            }
+        }
+        else
+        {
+            //transaction was not valid, add newly spent transaction outputs back to pool of unspent transactions
+            for(UTXO transactionToAddBackToPool : newlySpentTransactions.keySet())
+            {
+                unspentTransactions.addUTXO(transactionToAddBackToPool, newlySpentTransactions.get(transactionToAddBackToPool));
+            }
+        }
+
+        return validTransaction;
     }
 
     /**
@@ -28,7 +122,48 @@ public class TxHandler {
      * updating the current UTXO pool as appropriate.
      */
     public Transaction[] handleTxs(Transaction[] possibleTxs) {
-        // IMPLEMENT THIS
+        ArrayList<Transaction> potentialDoubleSpends = new ArrayList<>();
+        ArrayList<Transaction> validTransactions = new ArrayList<>();
+
+        //iterate through transactions once and save any potential double spends.
+        for(Transaction transaction : possibleTxs)
+        {
+            if(isValidTx(transaction))
+            {
+                //outputs from this transaction have been added to the newly created unspent transaction pool.
+                //old outputs that were expended in this transaction were removed from the unspent pool.
+                validTransactions.add(transaction);
+            }
+            else
+            {
+                //nothing was changed in the unspent transaction pool.
+                potentialDoubleSpends.add(transaction);
+            }
+        }
+
+        int potentialDoubleSpendCount = potentialDoubleSpends.size();
+        int oldPotentialDoubleSpendCount;
+
+        //iterate over any potential double spends after adding the unspent transactions of the confirmed transactions to the unspent set.
+        do {
+            for (Transaction potentialDoubleSpend : potentialDoubleSpends)
+            {
+                //add any that now return true to the set. This will maintain a valid ordering of the transactions as well if later transactions reference earlier ones
+                if(isValidTx(potentialDoubleSpend))
+                {
+                    validTransactions.add(potentialDoubleSpend);
+                    potentialDoubleSpends.remove(potentialDoubleSpend);
+                }
+            }
+
+            oldPotentialDoubleSpendCount = potentialDoubleSpendCount;
+            potentialDoubleSpendCount = potentialDoubleSpends.size();
+        } while (potentialDoubleSpendCount != oldPotentialDoubleSpendCount);
+        // If there were transactions added, iterate over the set again. Repeat until there is no change in the set.
+
+        Transaction[] finalValidTransactions = new Transaction[validTransactions.size()];
+        validTransactions.toArray(finalValidTransactions);
+        return finalValidTransactions;
     }
 
 }
